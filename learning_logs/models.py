3c8d7e2eeb1c38b495f8fe7e9a9a8f76de22256b
zgroves <eric@example.com>
2017-10-21 00:53:28 -0400

    Ready for deployment to heroku.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Topic(models.Model):
     '''A topic the user is learning about'''
     tex = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User)
     
     def __str__(self):
         '''return a string representation of the model'''
